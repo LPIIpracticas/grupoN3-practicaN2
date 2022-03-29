754d36b67631ff344b5f84e24ae159e354da5e6f
unknown <lpiipracticas2022@gmail.com>
2022-03-29 10:26:04 -0500

    ilusion con archivos  commit

---
+++ ilusion.cpp
@@ -0,0 +1,42 @@
+#include "ilusion.h"
+#include "fecha.h"
+#include "fecha.cpp"
+
+void Ilusion::inicio()
+{
+    cout << "Ingrese el numero de razones: ";
+    cin >> nroRazones;
+    razon = new string[nroRazones];
+    cin.ignore();
+    for (int i = 0; i < nroRazones;)
+    {
+        cout << "Ingrese la razon: ";
+        getline(cin, razon[i]);
+    }
+    cout << "Ingrese la fecha de inicio: ";
+    fechaInicio.ingresar();
+    cout << "Ingrese intensidad: ";
+    cin >> intensidad;
+}
+void Ilusion::final()
+{
+    fechaFin.ingresar();
+    intensidad = 0;
+    delete razon[];
+}
+void Ilusion::aumentar()
+{
+    intensidad++;
+}
+void Ilusion::disminuir()
+{
+    intensidad--;
+}
+void Ilusion::decepción()
+{
+    cout << "quiero morir..." << endl;
+}
+void Ilusion::confiar()
+{
+    cout << "pongolas manos al fuego..." << endl;
+}
